9aea8176496d9f11a535fb32c57685113cb1ffb6
amanyalfraidy-bos <amany.alfraidy@gmail.com>
2026-07-01 14:08:50 +0300

    Create prettier.config.js

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+export default {
+  semi: true,
+  singleQuote: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  printWidth: 100,
+  useTabs: false,
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  endOfLine: "lf",
+};
